2c6966e7cab5d01cc39d756db437d07cbe0f790c
Kyle <kyle@wellandlighthouse.com>
2014-06-06 08:58:52 -0400

    fucking around with access_tokens and authentication to gcal

--- event_trigger.py
+++ event_trigger.py
@@ -12,5 +12,17 @@ r = requests.post('https://accounts.google.com/o/oauth2/token', data=payload)
 
 json_resp = json.loads(r.content)
 access_token = json_resp['access_token']
+custom_headers = {'Authorization: Bearer': access_token}
 
-print access_token
+#authenticate with scope
+scope = 'https://www.googleapis.com/auth/calendar'
+
+#python script fires something off
+#now the actual calendar API actions...
+
+working_calendar_id = 'kyle@wellandlighthouse.com'
+
+payload = {'access_token':access_token}
+calendar = requests.get('https://www.googleapis.com/calendar/v3/calendars/'+working_calendar_id, data=payload)
+
+print calendar.url
