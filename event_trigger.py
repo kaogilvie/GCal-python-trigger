d762227769febad0b7e38d08282863c6228fd9eb
Kyle <kgilvi3@gmail.com>
2014-06-10 09:01:52 -0400

    access event objects; need to debug the time restrictions on get requests

--- event_trigger.py
+++ event_trigger.py
@@ -1,6 +1,7 @@
 import creds
 import requests
 import json
+import datetime
 
 payload = { 'refresh_token' : creds.refresh_token,
 			'client_id'		: creds.client_id,
@@ -20,9 +21,16 @@ scope = 'https://www.googleapis.com/auth/calendar'
 #python script fires something off
 #now the actual calendar API actions...
 
-working_calendar_id = 'foo'
+working_calendar_id = creds.working_calendar_id
 
 payload = {'access_token':access_token}
-calendar = requests.get('https://www.googleapis.com/calendar/v3/calendars/'+working_calendar_id, data=payload)
 
-print calendar.url
+search_query = 'Winner'
+startDate = str(datetime.datetime(2014,06,10))
+raw_input()
+
+query_params = '?access_token='+access_token+'&q='+search_query+'&timeMax=2014-06-09T00:00:00'
+
+calendar = requests.get('https://www.googleapis.com/calendar/v3/calendars/'+working_calendar_id+'/events'+query_params)
+
+print calendar.content
